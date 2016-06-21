0de0ea112d97182f748ad9be951f7ddfa994ddc8
iory <ab.ioryz@gmail.com>
2016-06-21 14:47:03 +0900

    Modified install cifar-10-data

--- install/download.sh
+++ install/download.sh
@@ -1,3 +1,8 @@
 #!/bin/sh -eu
 
+FILE_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+mkdir -p $FILE_DIRECTORY/../data
+cd $FILE_DIRECTORY/../data
+
 wget https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+tar xvzf cifar-10-python.tar.gz
